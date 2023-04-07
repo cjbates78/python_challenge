8f666a1b2e16707d16f464316512371fae97dc5f
cjbates78 <cjbates78@gmail.com>
2023-04-07 15:54:48 -0400

    docs:update assignment

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,61 +3,79 @@ import os
 import csv
 
 #specify the file to read 
-csvpath = os.path.join("election_data.csv")
+csvpath = os.path.join('PyPoll/Resources/election_data.csv')
 
+# create txt file to print to
+output_path = os.path.join("PyPoll.txt")
+
+#create a dictionary to store candidate votes
+candidate_count = {}
+
+#parameters
+ballots = []
+county = []
+candidate = []
+Charles_casper_Stockham = []
+Diana_DeGette = []
+Raymon_Anthony_Doane = []
+total_ballots = 0
+Stockham_ballot_count = 0
+DeGette_ballot_count = 0
+Doane_ballot_count = 0
 
 #csv reader specifies delimiter and variable that holds content
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
- #read the header row first
+    #read the header row first
     csv_header = next(csvreader)
-#Print Header
-    print("Election Results")
-    print("-------------------------------------------------------")
 
-#set inital value of counter to zero
-    rowcount = sum(1 for row in csvreader)
+    #read each row of data after the header
+    for row in csvreader:
 
-
-#find total votes and print
-    total_votes = rowcount
-    print("Total Votes: " + total_votes)
-    print("-------------------------------------------------------")
-
-#Loop thru and find canidates and count votes
-   
-canidate_votes = [2] in csvreader
-Stockham_votes = []
-DeGette_votes = []
-Doane_votes = []
-for votes in canidate_votes:
-    if canidate_votes =="Charles Casper Stockham":
-        Stockham_votes.append (+1)
-    elif canidate_votes =="Diana DeGette":
-        DeGette_votes.append (+1)
-    elif canidate_votes == "Raymon Anthony Doane":
-        Doane_votes.append (+1)
+        
+        
+        #find total votes and print
+        total_ballots += 1
+        
+        if row[2] =="Charles Casper Stockham":
+            Stockham_ballot_count += 1
+        elif row[2]== "Diana DeGette":
+            DeGette_ballot_count += 1
+        elif row[2] == "Raymon Anthony Doane":
+            Doane_ballot_count += 1
 
 
 #find canidate % of votes
-Stockham_percent = ((Stockham_votes / total_votes) * 100)
-DeGette_percent = ((DeGette_votes / total_votes) * 100)
-Doane_percent = ((Doane_votes / total_votes) * 100)
+    Stockham_percent =((Stockham_ballot_count / total_ballots) * 100)
+    DeGette_percent = ((DeGette_ballot_count / total_ballots) * 100)
+    Doane_percent = ((Doane_ballot_count / total_ballots) * 100)
 
-
-# print name: percent (total) x 3
-print("Charles Casper Stockham:" + Stockham_percent + "(" + Stockham_votes + ")")
-print("Diana DeGette:" + DeGette_percent + "(" + DeGette_votes +")")
-print("Raymon Anthony Doane: " + Doane_percent + "(" + Doane_votes + ")")
-print("-------------------------------------------------------")
+candidate_count = {"Charles Casper Stockham" : Stockham_ballot_count, "Diana DeGette" : DeGette_ballot_count, "Raymon Anthony Doane" : Doane_ballot_count}
 
 #find winner
-winner = max(canidate_votes)
-#print "Winner: " name
-print("-------------------------------------------------------")
+# taking list of car values in v
+v = list(candidate_count.values())
+ 
+# taking list of car keys in v
+k = list(candidate_count.keys())
+ 
+winner = (k[v.index(max(v))])
 
-# create txt file to print to
-output_path = os.path.join("PyPoll.txt")
-file = open("PyPoll.txt", "w")
+#generate output
+output = (
+    f"Election Results\n"
+    f"---------------------------------------\n"
+    f"Total Votes: {total_ballots} \n"
+    f"---------------------------------------\n"
+    f"Charles Casper Stockham: {Stockham_percent:.2f}% ({Stockham_ballot_count})\n"
+    f"Diana DeGette: {DeGette_percent:.2f}% ({DeGette_ballot_count})\n"
+    f"Raymon Anthony Doane: {Doane_percent:.2f}% ({Doane_ballot_count})\n"
+    f"---------------------------------------\n"
+    f"Winner: {winner} \n"
+    f"---------------------------------------\n")
+#print the output to terminal
+print(output)
+
+file = open("pypoll.txt", "w")
 file.close()
---
+++ pybank/main.py
@@ -0,0 +1,72 @@
+# Dependencies
+import os
+import csv
+
+#specify the file to read
+Budget_csv = os.path.join('C:/Users/Carrie Work/Desktop/python_challenge/pybank/Resources/budget_data.csv')
+                          
+# create txt file to print to
+output_path = os.path.join("pybank.txt")
+
+# Track various financial parameters
+total_months = 0
+month_of_change = []
+profit_loss = []
+greatest_increase = ["", 0]
+greatest_decrease = ["", 9999999999999999999]
+total_profit_loss = 0
+
+#csv reader specifies delimiter and variable that holds content
+with open(Budget_csv) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    #read the header row first
+    csv_header = next(csvreader)
+
+    # Extract first row to avoid appending to net_change_list
+    first_row = next(csvreader)
+    total_months += 1
+    total_profit_loss += int(first_row[1])
+    prev_net = int(first_row[1])
+
+        #start loop
+    for row in csvreader:
+            #add 1 to the count
+            total_months += 1
+
+            #add the first element in the total
+            total_profit_loss = int(row[1])
+
+            # Track the net change
+            net_change = int(row[1]) - prev_net
+            prev_net = int(row[1])
+            profit_loss += [net_change]
+            month_of_change += [row[0]]
+
+            # Calculate the greatest increase
+            if net_change > greatest_increase[1]:
+                greatest_increase[0] = row[0]
+                greatest_increase[1] = net_change
+
+            # Calculate the greatest decrease
+            if net_change < greatest_decrease[1]:
+                greatest_decrease[0] = row[0]
+                greatest_decrease[1] = net_change
+
+# Calculate the Average Net Change
+net_monthly_avg = sum(profit_loss) / len(profit_loss)
+
+# Generate Output Summary
+output = (
+    f"Financial Analysis\n"
+    f"----------------------------\n"
+    f"Total Months: {total_months}\n"
+    f"Total: ${total_profit_loss}\n"
+    f"Average Change: ${net_monthly_avg:.2f}\n"
+    f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})\n"
+    f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})\n")
+# Print the output (to terminal)
+print(output)
+
+file = open("pybank.txt", "w")
+file.close()
